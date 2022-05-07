e20064ce3bcb0548e065c50c5fe929eca1d74aeb
satishkumarojha <satishkumarojha15@gmail.com>
2022-05-07 20:57:41 +0530

    port changed

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,10 +2,10 @@ const app = require("./index");
 
 const connectdb = require("./configs/db")
 
-app.listen(5000, async () => {
+app.listen(6011, async () => {
     try{
         await connectdb();
-        console.log("listening on port 5000")
+        console.log("listening on port 6011")
     }
     catch(err){
         console.log(err.message);
